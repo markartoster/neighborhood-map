643758d5fa7886ab085228c8e1fd8dbab8684ca4
Karol Rosiński <markar.toster@gmail.com>
2018-07-21 19:25:40 +0200

    Init package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Main from './Main.js'
 import './App.css';
 
-class App extends Component {
+class App extends React.PureComponent {
+
+  state = {
+    isMarkerShown: true
+  }
+
   render() {
     return (
       <div>
-        
+        <Main
+          isMarkerShown = {this.state.isMarkerShown}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJTF1H_9xKdjSbmBkFz96KPCRRPaJfgQ0&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class Main extends Component {
-  
+class Main extends withScriptjs(withGoogleMap(Component)) {
+
   render() {
     return (
-
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      >
+        {this.props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+      </GoogleMap>
     );
   }
 }
