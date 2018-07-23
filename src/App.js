8f4797cdb645c793216b304db056e543511d391f
Karol Rosiński <markar.toster@gmail.com>
2018-07-23 17:24:40 +0200

    Moved state from Main Component to App Component(Root)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import './App.css';
 class App extends React.PureComponent {
 
   state = {
-    isMarkerShown: true
+    isMarkerShown: true,
+    interestingPoints: [
+      {name: "Cafe Strych", lat: 54.523280, lng: 18.543844},
+      {name: "Coffee Time", lat: 54.521288, lng: 18.532659},
+      {name: "TŁOK", lat: 54.516639, lng: 18.541942},
+      {name: "Starbucks", lat: 54.517589, lng: 18.541521},
+      {name: "Cyganeria", lat: 54.519417, lng: 18.535728},
+      {name: "Lavenda Cafe & Galeria", lat: 54.521259, lng: 18.540606}
+    ],
+    startingPosition: { lat: 54.5193926,lng: 18.5353235, zoom: 14.95}
   }
 
   render() {
@@ -19,6 +28,8 @@ class App extends React.PureComponent {
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
+          interestingPoints={this.state.interestingPoints}
+          startingPosition={this.state.startingPosition}
         />
 
       </div>
--- src/Main.js
+++ src/Main.js
@@ -3,16 +3,8 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class Main extends Component {
 
-  state = {
-    interestingPoints: [
-      {name: "Cafe Strych", lat: 54.523280, lng: 18.543844},
-      {name: "Coffee Time", lat: 54.521288, lng: 18.532659},
-      {name: "TŁOK", lat: 54.516639, lng: 18.541942},
-      {name: "Starbucks", lat: 54.517589, lng: 18.541521},
-      {name: "Cyganeria", lat: 54.519417, lng: 18.535728},
-      {name: "Lavenda Cafe & Galeria", lat: 54.521259, lng: 18.540606}
-    ],
-    startingPosition: { lat: 54.5193926,lng: 18.5353235, zoom: 14.95}
+  props = {
+  
   }
 
   componentDidMount() {
@@ -24,19 +16,19 @@ class Main extends Component {
       width: '100%',
       height: '100%'
     }
-    {this.state.interestingPoints.forEach((cafe) => {
+    {this.props.interestingPoints.forEach((cafe) => {
     console.log(cafe.name + " " + cafe.lng + " " + cafe.lat );
     })}
     return (
      <Map google={this.props.google}
-          zoom={this.state.startingPosition.zoom}
+          zoom={this.props.startingPosition.zoom}
           initialCenter={{
-            lat: this.state.startingPosition.lat,
-            lng: this.state.startingPosition.lng
+            lat: this.props.startingPosition.lat,
+            lng: this.props.startingPosition.lng
           }}
           style={style}
           >
-       {this.state.interestingPoints.map((cafe) => (
+       {this.props.interestingPoints.map((cafe) => (
          <Marker onClick={this.onMarkerClick}
                  key={cafe.name}
                  name={cafe.name}
